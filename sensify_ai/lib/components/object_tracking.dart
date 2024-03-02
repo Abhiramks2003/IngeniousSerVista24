df8a1fd3a5d8d058b725f32f585f6fa402a0901a
ABHIRAM KRISHNA S <abhiramkrishna085@gmail.com>
2024-03-02 19:49:13 +0530

    Created app

---
+++ sensify_ai/lib/components/object_tracking.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ObjectTracking extends StatefulWidget {
+  const ObjectTracking({super.key});
+
+  @override
+  State<ObjectTracking> createState() => _ObjectTrackingState();
+}
+
+class _ObjectTrackingState extends State<ObjectTracking> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
